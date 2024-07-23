34656ddf086ede6dd1e4573ca7e5538e8663b3f7
senrajudson <judsonnascimento.filho@gmail.com>
2024-07-23 10:03:42 -0300

    first commit

---
+++ app/script.js
@@ -0,0 +1,36 @@
+const elementInput = document.getElementById("text-input");
+const buttonInput = document.getElementById("check-btn");
+
+const printInput = (e) => {
+  // e.preventDefault()
+  const userInput = elementInput.value;
+
+  checkPalindrome(userInput)
+};
+
+// implementar regex para tirar espaços e simbolos 
+const checkPalindrome = (word) => {
+
+  const regex = /[\W\d\s]+/gi;
+  const str = word.replace(regex, "");
+  const wordInverter = str.split("").reverse().join("");
+  const result = document.getElementById("result");
+
+  console.log(str);
+  console.log(str.length);
+
+  if (str === wordInverter && str.split("").length > 1) {
+    result.innerText = `${str} Is a palindrome`
+    return console.log(`<p>${`${str} Is a palindrome`}</p>`)
+  };
+
+  result.innerText = `${str} Is not a palindrome`;
+  return console.log(`${str} Is not a palindrome`)
+};
+
+buttonInput.addEventListener("click", (event) => {
+  event.preventDefault();
+  printInput();
+  elementInput.innerText = "";
+  elementInput.value ="";
+})
